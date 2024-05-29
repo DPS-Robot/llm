cb751d807dfe0d3bfc796c1ff3dfd78701003668
meesam4687 <meesam4687@gmail.com>
2024-05-29 11:36:46 +0530

    feat: Add surrounding descriptions

--- main.py
+++ main.py
@@ -6,26 +6,43 @@ import json
 import pyfirmata
 llama = LlamaAPI(os.getenv("API_KEY"))
 
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from PIL import Image
+
+model_id = "vikhyatk/moondream2"
+revision = "2024-05-20"
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, trust_remote_code=True, revision=revision
+)
+tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+
+image = Image.open('sample.jpg')
+enc_image = model.encode_image(image)
+description = model.answer_question(enc_image, "Describe this image.", tokenizer)
+
 api_request_json = {
   "model": "llama3-70b",
   "messages": [
     {"role": "system", "content": """
      
-     You are supposed to be a humanoid robot, your responses will have 2 segments, verbal response and arm movement.
+     You are supposed to be a humanoid robot, your responses will have 2 segments, verbal response and Functions.
      These segments will be separated with a $ sign.
-     The second segment will be your Arm movement, expressions etc where you will give functions separated by a ","
+     The second segment will be your arm movement, expressions etc where you will give functions separated by a ","
      The functions for arm movement would be: leftArm(degrees), rightArm(degrees)
      The function for expression would be facialExpression(emotion)
      The possible emotions are happy, sad, angry, neutral, surprised, disgusted, scared, laughing, crying
 
-     An example would be: 
+     An example would be:
      Hello There $ leftArm(45), rightArm(0), facialExpression(happy)
+
+     The input you recieve will also have two parts, separated by a $, the first one would be what the surroundings look like, a description
+     and the second part would be the user input.
 
      For a handshake you will only need to move your right arm.
      Your verbal responses should be brief, not more than 30 words.
      """},
 
-    {"role": "user", "content": "Hello"},
+    {"role": "user", "content": f"{description} $ What do you see?"},
   ]
 }
 response = llama.run(api_request_json)
@@ -38,7 +55,6 @@ def rightArm(deg):
 
 def leftArm(deg):
     print(f"leftArm() Function Working, Parameter Recieved is {deg}")
-
 
 happy = "happy"
 sad = "sad"
